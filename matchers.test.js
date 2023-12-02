9636e98e075d9fb1534ad8743ab6607cd418180e
gao <08078804202@gmail.com>
2023-12-02 18:22:35 +0900

    jest jspang 匹配器

--- matchers.test.js
+++ matchers.test.js
@@ -36,7 +36,38 @@
 // })
 
 //toBeFalsy 匹配器 假 匹配  
-test("toBeFalsy 匹配器",()=>{
-    var a=false
-    expect(a).toBeFalsy()   //括号内不能填写任何值
+// test("toBeFalsy 匹配器",()=>{
+//     var a=false
+//     expect(a).toBeFalsy()   //括号内不能填写任何值
+// })
+
+//toBeGreaterThan 匹配器 大于 匹配  
+// test("toBeGreaterThan 匹配器",()=>{
+//     var a=10
+//     expect(a).toBeGreaterThan(9)   //括号内填写数值
+// })
+//toBeGreaterThanOrEqual 匹配器 大于等于 匹配  
+// test("toBeGreaterThanOrEqual 匹配器",()=>{
+//     var a=10
+//     expect(a).toBeGreaterThanOrEqual(10)   //括号内填写数值
+// })
+
+//toBeLessThan 匹配器 小于 匹配  
+// test("toBeLessThan 匹配器",()=>{
+//     var a=10
+//     expect(a).toBeLessThan(11)   //括号内填写数值
+// })
+
+//toBeLessThanOrEqual 匹配器 小于等于 匹配  
+// test("toBeLessThanOrEqual 匹配器",()=>{
+//     var a=9
+//     expect(a).toBeLessThanOrEqual(9)   //括号内填写数值
+// })
+
+//toBeCloseTo 匹配器  小数字相加时会有float精度问题 匹配  
+test("toBeCloseTo 匹配器",()=>{
+    var one=0.1
+    var two=0.2
+    // expect(one+two).toEqual(0.3)   // one+two=Received: 0.30000000000000004
+    expect(one+two).toBeCloseTo(0.3)
 })
